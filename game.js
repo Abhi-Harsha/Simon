c9a122b3797e7c71217bfd68ea6b1c0dbba5a1f9
abhishek.h.aux <abhishek.h.aux@go-jek.com>
2020-03-01 00:35:31 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,71 @@
+var gameStarted = false;
+var gameInProgress = false;
+
+var buttons = { green: 0, red: 1, yellow: 2, blue: 3 };
+var generatedNumbers = [];
+var userClickedNumbers = [];
+var userGameLevelCounter = 0;
+
+$(".btn").click(buttonClicked);
+$(document).keyup(handleGameStatus);
+
+function generateNum() {
+  if (gameStarted) {
+    var generatedNum = Math.floor(Math.random() * 4);      // returns a random integer from 0 to 3
+    console.log("Generated num " + generatedNum);
+    generatedNumbers.push(generatedNum);
+  }
+}
+
+function handleGameStatus() {
+  if (!gameStarted) {
+    gameStarted = true;
+    updateLevel();
+  }
+}
+
+function restartGame() {
+  gameStarted = false;
+  gameInProgress = false;
+  $("#level-title").text("Press Any key to start");
+  userClickedNumbers = [];
+  generatedNumbers = [];
+}
+
+function updateLevel() {
+  generateNum();
+  userClickedNumbers = [];
+  userGameLevelCounter = 0;
+  $("#level-title").text("Level " + generatedNumbers.length);
+}
+
+function handleButtonClick(buttonId) {
+  userClickedNumbers.push(buttonId);
+  console.log("gen num : " + generatedNumbers[userGameLevelCounter] + "user click : " + userClickedNumbers[userGameLevelCounter]);
+  if (generatedNumbers[userGameLevelCounter] === userClickedNumbers[userGameLevelCounter]) {
+    if(userGameLevelCounter === generatedNumbers.length - 1) {
+      updateLevel();
+    } else {
+      userGameLevelCounter += 1;
+    }
+  } else {
+    restartGame();
+  }
+}
+
+function buttonClicked(event) {
+  switch (event.target.id) {
+    case "green":
+      handleButtonClick(buttons.green);
+      break;
+    case "red":
+      handleButtonClick(buttons.red);
+      break;
+    case "yellow":
+      handleButtonClick(buttons.yellow);
+      break;
+    case "blue":
+      handleButtonClick(buttons.blue);
+      break;
+  }
+}
